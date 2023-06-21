5f3d2c07d671de164f8ce03785763dc7d383a077
id <id@AQ-0039>
2023-06-21 16:42:55 +0900

    あ

--- src/test02/sample01.java
+++ src/test02/sample01.java
@@ -4,7 +4,7 @@ public class sample01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("むらさきだぴょ～ん^_^");
+		System.out.println("むらさきだぴょ～ん^_^←？");
 		System.out.println("伊山ぐう有能");
 	}
 
